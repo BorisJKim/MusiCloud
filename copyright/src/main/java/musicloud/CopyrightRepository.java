13ec053a74b0697d437ab4cade375efcf1545560
Boris J. Kim <fstgzero@naver.com>
2021-02-04 11:08:47 +0900

    Delete copyright directory
    소스 다시 올리기

--- copyright/src/main/java/musicloud/CopyrightRepository.java
+++
@@ -1,8 +0,0 @@
-package musicloud;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CopyrightRepository extends PagingAndSortingRepository<Copyright, Long>{
-
-
-}
