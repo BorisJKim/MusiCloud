53692c06b9fa08cdc46df4050c09d8f549ab309f
Boris J. Kim <fstgzero@naver.com>
2021-02-04 11:11:44 +0900

    Add files via upload
    소스 다시 올리기

---
+++ source/src/main/java/musicloud/SourceRepository.java
@@ -0,0 +1,8 @@
+package musicloud;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SourceRepository extends PagingAndSortingRepository<Source, Long>{
+
+
+}
