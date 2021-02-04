86ed66f62e283fedac5036f9fce410ba3352885c
Boris J. Kim <fstgzero@naver.com>
2021-02-05 03:20:06 +0900

    Create Approved.java

---
+++ content/src/main/java/musicloud/Approved.java
@@ -0,0 +1,48 @@
+package musicloud;
+
+public class Approved extends AbstractEvent {
+
+    private Long id;
+    private Long contentId;
+    private String status;
+    private String artistName;
+    private String musicTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getContentId() {
+        return contentId;
+    }
+
+    public void setContentId(Long contentId) {
+        this.contentId = contentId;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+    
+    public String getMusicTitle() {
+        return musicTitle;
+    }
+
+    public void setMusicTitle(String musicTitle) {
+        this.musicTitle = musicTitle;
+    }
+}
