bc58266a7d69f5cf2dee2e79c3dcf67c627651bf
Boris J. Kim <fstgzero@naver.com>
2021-02-04 11:08:33 +0900

    Delete content directory
    소스 다시 올리기

--- content/src/main/java/musicloud/ContentRepository.java
+++
@@ -1,8 +0,0 @@
-package musicloud;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ContentRepository extends PagingAndSortingRepository<Content, Long>{
-
-
-}
