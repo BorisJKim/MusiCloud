7d9d1f651937195f5f3fde1bc2f74e968cac857e
Boris J. Kim <fstgzero@naver.com>
2021-02-04 11:09:24 +0900

    Delete source directory
    소스 다시 올리기

--- source/src/main/java/musicloud/SourceRepository.java
+++
@@ -1,8 +0,0 @@
-package musicloud;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface SourceRepository extends PagingAndSortingRepository<Source, Long>{
-
-
-}
